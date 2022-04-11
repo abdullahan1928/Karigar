d9099bfd38feb982e78f54b49996eaacfa9a02b2
Abdullah <abdullahan1928@gmail.com>
2022-04-12 01:23:19 +0500

    Added Services in new js file

---
+++ client/src/Services/allServices.js
@@ -0,0 +1,59 @@
+import AC from '../images/AC.jpg';
+import ACbox from '../images/AClogo.png'
+import electricianLogo from '../images/electrician.png';
+import Electrician from '../images/electric.jpg';
+import Carpenter from '../images/Carpenter.jpg';
+import CarpenterLogo from '../images/wood.png';
+import plumberLogo from '../images/plumber.png';
+import Plumber from '../images/plumber.jpg';
+import painterLogo from '../images/painterBack.png';
+import Painter from '../images/painter1.jpg';
+import More from '../images/More1.jpg';
+import moreLogo from '../images/more.png';
+
+let Servic = [
+    {
+        "title": "AC Service",
+        "status": "Any type of service",
+        "description": "AC Serivices which include AC repaaring cleaning and much more",
+        "image": AC,
+        "logo": ACbox
+    },
+    {
+        "title": "Electrician Services",
+        "status": "Any type of service",
+        "description": "Electrician Services include any kind of services related to Electricity",
+        "image": Electrician,
+        "logo": electricianLogo
+    },
+    {
+        "title": "Carpenter Service",
+        "status": "Any type of service",
+        "description": "Any Services related to Wood. Our top qualified Carpenters are available",
+        "image": Carpenter,
+        "logo": CarpenterLogo
+    },
+    {
+        "title": "Plumbing Service",
+        "status": "Any type of service",
+        "description": "Any kind of taks related to plumbing will be done with full experty.",
+        "image": Plumber,
+        "logo": plumberLogo
+    },
+    {
+        "title": "Painting Service",
+        "status": "Any type of service",
+        "description": "If you are looking for one of the finest painter of the town",
+        "image": Painter,
+        "logo": painterLogo
+    },
+    {
+        "title": "More Services",
+        "status": "Many kind of services Mechanic,Washing and more...",
+        "description": "Many sort of more services",
+        "image": More,
+        "logo": moreLogo
+    }
+]
+
+export default Servic
