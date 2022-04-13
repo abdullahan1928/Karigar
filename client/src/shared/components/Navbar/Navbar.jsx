f2099ad12d3d5ed4e815ead3a63cce3e5c40c36b
Abdullah <abdullahan1928@gmail.com>
2022-04-14 02:46:27 +0500

    Mapped Service card services

--- client/src/shared/components/Navbar/Navbar.jsx
+++ client/src/shared/components/Navbar/Navbar.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 const Navbar = (props) => {
   return (
-    <nav class="navbar" style={props.additionalStyles ? props.additionalStyles : {}}>
+    <nav className="navbar" style={props.additionalStyles ? props.additionalStyles : {}}>
 
-      <ul class="nav-links" >
-        <div class="menu" style={props.additionalMenuStyles ? props.additionalMenuStyles : {}}>
+      <ul className="nav-links" >
+        <div className="menu" style={props.additionalMenuStyles ? props.additionalMenuStyles : {}}>
           <li style={props.additionalHoverStyles ? props.additionalHoverStyles : {}} className='hoverEffect'>
             <Link to="/" className='links' style={props.additionalLinkStyles ? props.additionalLinkStyles : {}}>
               <p className='hoverText' style={props.additionalHoverStyles ? props.additionalHoverStyles : {}}>Home</p>
