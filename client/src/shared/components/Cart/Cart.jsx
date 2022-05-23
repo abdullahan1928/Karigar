a7abcb60584d502a54a97dfcbeb037b5f23914e5
Abdullah <abdullahan1928@gmail.com>
2022-05-24 03:04:08 +0500

    Cart System added

---
+++ client/src/shared/components/Cart/Cart.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Cart.css'
+
+export default function Cart({ cartItems }) {
+
+    return (
+        <>
+            <h2>Cart Items</h2>
+            <div>
+                <div> {cartItems.length === 0 && <div>Cart is empty</div>}</div>
+                {cartItems.map((item) =>
+                    <li key={item.id} className="row">
+                        {item.id}
+                        <div className="col-2">{item.title}</div>
+                        <div className="col-2">{item.description}</div>
+                        <div className="col-2">{item.price}</div>
+                    </li>
+                )}
+            </div>
+        </>
+    );
+
+}
