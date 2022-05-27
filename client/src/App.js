423ee5e4d85eb7fc16c86c9544a9cf551cd8e354
Abdullah <abdullahan1928@gmail.com>
2022-05-27 21:37:59 +0500

    Cart Updated

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Profile from './pages/Dashboard/Profile/Profile';
 import { userInputs } from './formSource';
 import PreviousOrders from './pages/Dashboard/PreviousOrders/PreviousOrders';
 import OrdersInQueue from './pages/Dashboard/OrdersInQueue/OrdersInQueue';
+import Checkout from './pages/Subservices/Checkout/Checkout';
 
 function App() {
   return (
@@ -23,20 +24,23 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/service" >
           <Route index element={<Services />} />
-          <Route path=":title" element={<SubService />} />
-        </Route>
+          <Route path=":title" >
+            <Route index element={<SubService />} />
+            <Route path="checkout" element={<Checkout />} />
+          </Route >
+        </Route >
         <Route path="/contact" element={<Contact />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" >
           <Route index element={<Dashboard />} />
-          <Route path="profile" element={<Profile inputs={userInputs} title="Profile" />} />
+          <Route path="profile" element={<Profile inputs={userInputs} />} />
           <Route path="previousorders" element={<PreviousOrders />} />
           <Route path="ordersinqueue" element={<OrdersInQueue />} />
         </Route>
       </Routes>
       <AlertNotifications />
-    </Router>
+    </Router >
   );
 }
 
--- client/src/shared/components/Cart/Cart.jsx
+++ client/src/shared/components/Cart/Cart.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import './Cart.css'
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 
 export default function Cart({ cartItems }) {
 
     return (
         <>
             <h2>Cart Items</h2>
-            <div>
-                <div> {cartItems.length === 0 && <div>Cart is empty</div>}</div>
-                {cartItems.map((item, index) =>
-                    <div key={index} className="row">
-                        {item.id}
-                        <div className="col-2">{item.title}</div>
-                        {/* <div className="col-2">{item.description}</div> */}
-                    </div>
-                )}
-            </div>
+            <SimpleBar
+                style={{ maxHeight: 430 }}
+            >
+                <div>
+                    <div> {cartItems.length === 0 && <div>Cart is empty</div>}</div>
+                    {cartItems.map((item, index) =>
+                        <div key={index} className="row">
+                            {item.id}
+                            <div className="col-2">{item.title}</div>
+                            <div className="col-2">{item.description}</div>
+                        </div>
+                    )}
+                </div>
+            </SimpleBar>
         </>
     );
 
